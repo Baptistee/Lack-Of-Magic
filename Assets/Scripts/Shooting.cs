56dcd7943f9b068991b104c1a39e7446eb431bd5
kevinhuart <huart.ke@gmail.com>
2020-02-15 11:57:34 +0100

    Ajout des controles de bases
    Ajout :
    - déplcements
    - rotation du perso vers le curseur
    - tir de projectile vers le curseur

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject projectile;
+
+    public float projectileSpeed = 15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject proj = Instantiate(projectile, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = proj.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.right * projectileSpeed, ForceMode2D.Impulse);
+    }
+}
